5b7fab7a8945d3def1e33e071f2f55f2905f491d
Velmurugan Sivaprakasam <velmurugansivaprakasam89@gmail.com>
2021-10-14 19:53:41 +0530

    14/10/2021: Font Change Build issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -175,20 +175,5 @@ module.exports = {
         }
       }
     },
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Roboto Mono`,
-            variants: [`400`, `700`]
-          },
-          {
-            family: `Roboto`,
-            subsets: [`latin`]
-          },
-        ],
-      },
-    }
   ],
 }
